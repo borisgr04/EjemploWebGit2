32908ab68e5dfb986517945359b2d864045832dc
borisgr04 <borisgr04@hotmail.com>
2020-03-26 16:46:05 -0500

    quito comentarios innecesarios del startip

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-//ejemplo de cambio
+
 namespace EjemploWebGit
 {
     public class Startup
